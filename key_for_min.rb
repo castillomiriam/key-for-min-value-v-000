5b5c99daa0036996fdafeeb905b5b04aeddacf04
Miriam Castillo <castillo.m.mc@gmail.com>
2018-07-19 04:46:55 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 def key_for_min_value(name_hash)
   name_hash = {
-      "Timmy" => 9, 
-      "Sarah" => 6, 
+      "Timmy" => 9,
+      "Sarah" => 6,
       "Amanda" => 27
   }
-   
+
   key_for_min_value.collect do |kids_name, age|
       age
   end
